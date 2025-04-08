fe39865cd6e1aea24725c0afedd845bcb2113721
Kauet Dias <kauetdiaz@gmail.com>
2025-04-08 17:26:31 -0300

    tentando arrumar deploy

--- render-deploy.sh
+++ render-deploy.sh
@@ -1,12 +1,5 @@
-
-#! /usr/bin/env bash
-
 set -e 
 
 poetry install --no-root
-poetry lock --no-update
-poetry run flask --app src.app db init 
-poetry run flask --app src.app db migrate -m "remake"
-poetry run flask --app src.app db upgrade 
+poetry run flask --app src.app db upgrade
 poetry run gunicorn src.wsgi:app
-
